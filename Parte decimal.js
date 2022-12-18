9d9f125aacba12af1c6e3a9480e6a5b585bf5937
Jesus053 <118015681+Jesus053@users.noreply.github.com>
2022-12-17 18:05:18 -0600

    Add files via upload

---
+++ Parte decimal.js
@@ -0,0 +1,7 @@
+let numeroentero;
+numeroentero=prompt("ingresa un numero con decimal");
+    let variableE = parseInt(numeroentero);
+    let variableD = parseFloat(numeroentero);
+    r = Math.abs(variableD-variableE);
+    
+console.log(r)
---
+++ Pitagoras.js
@@ -0,0 +1,8 @@
+let a;
+let b;
+let c;
+    a = prompt("ingresa cateto a");
+    b = prompt("ingresa cateto b");
+
+c = Math.sqrt(a**2+b**2);
+console.log(c)
