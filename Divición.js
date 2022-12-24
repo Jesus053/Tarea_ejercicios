6d34b1ab7103b153020d097e5c74bce87bf84453
Jesus053 <118015681+Jesus053@users.noreply.github.com>
2022-12-24 17:26:15 -0600

    Add files via upload

---
+++ Divición.js
@@ -0,0 +1,11 @@
+let valor1;
+let valor2;
+let valor3;
+    valor1= prompt ("ingresa el numero a dividir");
+    valor2= prompt("ingresa el dividendo");
+
+oper = valor1/valor2;
+valor3=valor1%valor2;
+    if(valor3==0){console.log("la division es exacta")}
+    else(console.log("la divicion no es exacta"))
+console.log(oper)
